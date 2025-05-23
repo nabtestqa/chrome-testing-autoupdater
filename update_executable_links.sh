cac2b1f12bb8c8ea946a15872dc618dce37827e5
nabtestqa <narek.abrahamiann@gmail.com>
2025-05-23 19:52:34 +0400

    Update update_executable_links.sh

--- update_executable_links.sh
+++ update_executable_links.sh
@@ -3,11 +3,11 @@
 PWD=$(pwd)
 MTO="/usr/local/bin"
 
-rm "$MTO/chrome-testing"
-ln -s "$PWD/chrome-testing/chrome-linux64/chrome" "$MTO/chrome-testing"
+rm "$MTO/chrome"
+ln -s "$PWD/chrome-testing/chrome-linux64/chrome" "$MTO/chrome"
 
-rm "$MTO/chrome-testing-headless"
-ln -s "$PWD/chrome-testing-headless-shell/chrome-headless-shell-linux64/chrome-headless-shell" "$MTO/chrome-testing-headless"
+rm "$MTO/chrome-headless-shell"
+ln -s "$PWD/chrome-testing-headless-shell/chrome-headless-shell-linux64/chrome-headless-shell" "$MTO/chrome-headless-shell"
 
-rm "$MTO/chromedriver-testing"
-ln -s "$PWD/chromedriver/chromedriver-linux64/chromedriver" "$MTO/chromedriver-testing"
+rm "$MTO/chromedriver"
+ln -s "$PWD/chromedriver/chromedriver-linux64/chromedriver" "$MTO/chromedriver"
